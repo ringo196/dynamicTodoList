8e3f2f1a4379d1b988c96917b13c439fd53c021f
ringo_sanchez <ringo196@gmail.com>
2019-02-23 20:28:44 -0800

    Implement basic logic of conditional rendering for task entry completion status

--- src/TaskEntry.js
+++ src/TaskEntry.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TaskEntry =({ task }) => {
+class TaskEntry extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      status: 'incomplete',
+    }
+  }
 
-  console.log('taskengry', task)
-  return (
-    <div className="TaskEntryContainer">
-      <div className="imageContainer">
+  getStatus(){
+    if(this.props.task.completedAt){
+      this.setState({status: 'completed'})
+    } else {
+      this.props.taskList.forEach((task) => {
+        if(this.props.task.dependencyIds.includes(task.id)){
+          if(!task.completedAt){
+            this.setState({ status: 'locked' })
+          }
+        }
+      })
+    }
+  }
+
+  componentDidMount(){
+    this.getStatus();
+  }
+
+  render(){
+    let status;
+    if(this.state.status === 'completed'){
+       status = <div>completed</div>
+    }
+    if(this.state.status === 'incomplete'){
+       status = <div>incomplete</div>
+    }
+    if(this.state.status === 'locked'){
+       status = <div>locked</div>
+    }
+
+
+    return (
+      <div className="TaskEntryContainer">
+        <div className="imageContainer">
+        </div>
+        {status}
+        <div className="taskTextContainer"> {this.props.task.task}
+        </div>
       </div>
-      <div className="taskTextContainer"> {task.task}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TaskEntry;
