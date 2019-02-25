ff5fbb07a311bbcfa48a72701ae3ce69067b8596
ringo_sanchez <ringo196@gmail.com>
2019-02-24 19:19:02 -0800

    Add click handler and logic for toggling user clicks on tasks for completion

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TaskEntry from './TaskEntry.js'
 
-const TaskList =({ taskList, page, taskGroupClickHandler }) => {
+const TaskList =({ taskList, page, taskGroupClickHandler, taskStatusClickHandler }) => {
   return (
     <div>
       <div className="TaskGroupPage">{page}</div>
         <div onClick={taskGroupClickHandler}>All Groups</div>
         {taskList.filter((task) => task.group === page)
         .map( task => {
-          return <TaskEntry taskList={ taskList } className="TaskEntry" task={ task } key={ task.id } />
+          return <TaskEntry className="TaskEntry" taskList={ taskList } task={ task } key={ task.id } taskStatusClickHandler={taskStatusClickHandler} />
       })}
     </div>
   )
